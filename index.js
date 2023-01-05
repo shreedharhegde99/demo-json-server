5958788d022767c96350f15d3606b6a2a2eb8126
Shreedhar Hegde <snhkelemane@gmail.com>
2023-01-05 15:08:32 +0530

    changed cors config

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const jsonServer = require("json-server");
 const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middleware = jsonServer.defaults();
+const middleware = jsonServer.defaults({ noCors: true });
 const port = process.env.port || 3001;
 
+server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
   res.setHeader("access-control-allow-origin", "*");
   next();
 });
-server.use(jsonServer.bodyParser());
 server.use(router);
 server.use(middleware);
 server.use(cors({ origin: "*" }));
