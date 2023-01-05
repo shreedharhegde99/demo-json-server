7aa923859a89b871bc24298777b2d11427a5e7e6
Shreedhar Hegde <snhkelemane@gmail.com>
2023-01-05 12:29:56 +0530

    added cors config

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middleware = jsonServer.defaults();
@@ -6,6 +7,7 @@ const port = process.env.port || 3001;
 
 server.use(router);
 server.use(middleware);
-server.listen(port,()=>{
-    console.log(`Server is up and running at port: ${port}`)
-})
+server.use(cors());
+server.listen(port, () => {
+  console.log(`Server is up and running at port: ${port}`);
+});
