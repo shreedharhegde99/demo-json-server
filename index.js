39a89b589b8d425740e639e94f98c18dd48f1b01
Shreedhar Hegde <snhkelemane@gmail.com>
2023-01-05 15:16:52 +0530

    reverted cord config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middleware = jsonServer.defaults({ noCors: true });
+const middleware = jsonServer.defaults();
 const port = process.env.port || 3001;
 
 server.use(jsonServer.bodyParser);
