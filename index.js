01e673700a7854c20edbba191e9897f9d5928ad6
Shreedhar Hegde <snhkelemane@gmail.com>
2023-01-05 14:52:10 +0530

    changed server config

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const router = jsonServer.router("db.json");
 const middleware = jsonServer.defaults();
 const port = process.env.port || 3001;
 
-server.use(router);
-server.use(middleware);
-server.use(cors({ origin: "*" }));
 server.use((req, res, next) => {
   res.setHeader("access-control-allow-origin", "*");
   next();
 });
+server.use(router);
+server.use(middleware);
+server.use(cors({ origin: "*" }));
 server.listen(port, () => {
   console.log(`Server is up and running at port: ${port}`);
 });
