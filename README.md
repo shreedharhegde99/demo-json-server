bec51ba8bc600b34dacd097e9ef149e83ec81e96
Shreedhar Hegde <snhkelemane@gmail.com>
2023-01-07 00:07:29 +0530

    updated file structure

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ### Server is deployed on 
 ```
-https://scarlet-puffer-gown.cyclic.app/
+https://scarlet-puffer-gown.cyclic.app
+https://json-server-zk5a.onrender.com
 ```
--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 const jsonServer = require("json-server");
 const cors = require("cors");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("src/db.json");
 const middleware = jsonServer.defaults();
 const port = process.env.port || 3001;
 
+server.use(cors());
 server.use(jsonServer.bodyParser);
-server.use((req, res, next) => {
-  res.setHeader("access-control-allow-origin", "*");
-  next();
-});
 server.use(router);
 server.use(middleware);
-server.use(cors({ origin: "*" }));
 server.listen(port, () => {
   console.log(`Server is up and running at port: ${port}`);
 });
